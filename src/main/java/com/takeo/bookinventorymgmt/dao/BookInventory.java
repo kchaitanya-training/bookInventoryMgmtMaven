f2c962231041c38b112d445b6f3eee76d15c492b
krishna chaitanya <kchaitanya.48@outlook.com>
2023-11-17 02:00:31 +0530

    database changes done

--- src/main/java/com/takeo/bookinventorymgmt/dao/BookInventory.java
+++ src/main/java/com/takeo/bookinventorymgmt/dao/BookInventory.java
@@ -1,11 +1,14 @@
 package com.takeo.bookinventorymgmt.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import com.takeo.bookinventorymgmt.exception.BookInventoryException;
 import com.takeo.bookinventorymgmt.model.Book;
+import com.takeo.bookinventorymgmt.util.DBConnection;
 
 public class BookInventory {
 	
@@ -16,11 +19,22 @@ public class BookInventory {
 	}
 	
 	public void addBook(Book book) throws BookInventoryException{
+		System.out.println("book "+book.getBookName());
+		try {
+			Connection con=DBConnection.getConnection();
+		PreparedStatement ps=	con.prepareStatement("");//Insert query
+		ps.execute();
+			con.close();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		
-		
-		
-		
+			
 	}
 	
 	public List<Book> getBook(){
